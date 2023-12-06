5d4ff5db802c11928d8129dff15c67794936822e
bjchon <69482412+bjchon@users.noreply.github.com>
2023-12-05 20:57:34 -0500

    update style and cover letter and resume

--- js/githubIcon.js
+++ js/githubIcon.js
@@ -1,6 +1,6 @@
 var animation = bodymovin.loadAnimation({
     container: document.getElementById('githubContainer'),
-    path: 'uploads/icons8-github.json',
+    path: 'uploads/icons8-github2.json',
     renderer: 'svg',
     loop: true,
     autoplay: true,
